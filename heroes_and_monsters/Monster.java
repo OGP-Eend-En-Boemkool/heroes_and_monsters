b4b460c972c7b35da5d7877d9742c8a80205e511
Linde <Linde@10.46.235.151>
2017-05-17 17:09:31 +0200

    Protection in monster

--- heroes_and_monsters/Monster.java
+++ heroes_and_monsters/Monster.java
@@ -221,4 +221,131 @@ public class Monster extends Creature implements Damage {
 	public boolean isValidMaximumDamage(int damage) {
 		return (damage >= 1);
 	}
+
+	/**************************************
+	 * Protection
+	 **************************************/
+	
+	/**
+	 * Variable registering the value of the current damage.
+	 */
+	public int protection = 1;
+	
+	/**
+	 * Variable registering the maximum value of the damage.
+	 */
+	public final int maxProtection = 100;
+	
+	/**
+	 * Returns the current value for the protection of a monster.
+	 * 
+	 * @return The resulting number must be bigger than or equal to 1 and smaller than the maximum protection.
+	 * 		   | (result >= 1) && (result <= maxProtection)
+	 */
+	@Override
+	public int getCurrentProtection() {
+		return this.protection;
+	}
+
+	/**
+	 * Sets the current protection to the given number.
+	 * 
+	 * @param protection
+	 * 		  The number to which the protection needs to be set.
+	 * @post  The given number must be a legal number.
+	 * 		  | canHaveAsProtection(protection)
+	 * @post  The number to which the protection is set is equal to the given number.
+	 * 		  | new.getCurrentProtection().equals(protection) 
+	 */
+	@Override
+	public void setCurrentProtection(int protection) {
+		if (this.canHaveAsProtection(protection)){
+			this.protection = protection;
+		}
+	}
+
+	/**
+	 * Decreases the current protection with a given integer 'decrease'.
+	 * 
+	 * @param  decrease
+	 * 		   The integer we use to decrease the current protection with.
+	 * @post   The new protection is the original protection decreased with an integer 'decrease'.
+	 * 		   | new.getCurrentProtection().equals(this.getCurrentProtection() - decrease)
+	 * @post   The new protection is smaller than the original protection.
+	 * 		   | new.getCurrentProtection() < this.getCurrentProtection()
+	 * @post   The new protection must be a legal case
+	 * 	       | canHaveAsProtection(new.getCurrentprotection())
+	 * @throws IllegalArgumentException
+	 * 		   Throws an IllegalArgumentException when the integer 'decrease' is not positive or when 
+	 * 		   'decrease' is bigger or equal to the original protection.
+	 * 		   | if ((decrease <= 0)||(decrease >= this.getCurrentProtection()))
+	 * 		   | 	throw new IllegalArgumentException()
+	 * @effect The new protection of this object is set to the original protection decreased with 'decrease'
+	 * 		   | setCurrentProtection(this.getCurrentProtection() - decrease)
+	 */
+	@Override
+	public void decreaseProtection(int decrease) throws IllegalArgumentException {
+		if (decrease > 0){
+			int decreasedProtection = this.protection - decrease;
+			if (canHaveAsProtection(decreasedProtection)){
+				this.setCurrentProtection(decreasedProtection);
+			}
+			else {
+				throw new IllegalArgumentException("Invalid protection reached");
+			}
+		}
+		else {
+			throw new IllegalArgumentException("Use a positive number to decrease.");
+		}
+	}
+
+	/**
+	 * Increases the current protection with a given integer 'increase'.
+	 * 
+	 * @param  increase
+	 * 		   The integer we use to increase the current protection with.
+	 * @post   The new protection is the original protection increased with an integer 'increase'.
+	 * 		   | new.getCurrentProtection().equals(this.getCurrentProtection() + increase)
+	 * @post   The new protection is bigger than the original protection.
+	 * 	  	   | new.getCurrentProtection() > this.getCurrentProtection()
+	 * @post   The new protection must be a legal case
+	 * 	       | canHaveAsProtection(new.getCurrentprotection())
+	 * @throws IllegalArgumentException
+	 * 		   Throws an IllegalArgumentException when the integer 'increase' is not positive
+	 *  	   or when 'increase' is bigger than the maximum protection decreased with the original 
+	 *  	   protection.
+	 *  	   | if ((increase <= 0)||(increase >= (this.maxProtection() - this.getCurrentProtection())))
+	 * @effect The new protection of this object is set to the original protection increased with 'increase'
+	 * 		   | setCurrentProtection(this.getCurrentProtection() + increase)
+	 */
+	@Override
+	public void increaseProtection(int increase) throws IllegalArgumentException {
+		if (increase > 0){
+			int increasedProtection = this.protection + increase;
+			if (canHaveAsProtection(increasedProtection)){
+				this.setCurrentProtection(increasedProtection);
+			}
+			else {
+				throw new IllegalArgumentException("Invalid protection reached.");
+			}
+		}
+		else {
+			throw new IllegalArgumentException("Use a positive number to increase.");
+		}
+	}
+
+	/**
+	 * Checks whether or not the given protection is a legal case.
+	 * 
+	 * @param  protection
+	 * 		   The integer that needs to be checked.
+	 * @return True if the given integer is bigger than 0 and smaller than the maximum Protection, 
+	 * 		   false otherwise.
+	 * 		   | result == ((protection >= 1) && (protection <= this.maxProtection))
+	 */
+	@Override
+	public boolean canHaveAsProtection(int protection) {
+		return ((protection >= 1)&&(protection <= this.maxProtection));
+	}
+
 }
